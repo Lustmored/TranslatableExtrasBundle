2330df9388278ca2f1ca75b11dcc3144e9f4f5f5
Jakub Caban <kuba.iluvatar@gmail.com>
2021-09-07 12:02:06 +0200

    Fix bundle::getPath

--- src/TranslatableExtrasBundle.php
+++ src/TranslatableExtrasBundle.php
@@ -7,5 +7,8 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class TranslatableExtrasBundle extends Bundle
 {
-
+    public function getPath(): string
+    {
+        return \dirname(__DIR__);
+    }
 }
