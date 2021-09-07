db309c76a43c84f5eb401771d47e8b468220f8e9
Jakub Caban <kuba.iluvatar@gmail.com>
2021-09-07 11:15:31 +0200

    Cleanup

---
+++ src/TranslatableExtrasBundle.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace Lustmored\TranslatableExtrasBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TranslatableExtrasBundle extends Bundle
+{
+
+}
